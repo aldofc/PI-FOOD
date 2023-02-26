3f24efe879fe6c694330934cfa2ecb8eee43ec10
aldofc <aldofc18@gmail.com>
2023-02-25 23:19:39 -0600

    first commit

---
+++ api/src/models/Diets.js
@@ -0,0 +1,23 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('Diets', {
+
+  id:{
+    type: DataTypes.UUID,  //esto es para no tener conflictos con los id's de la api que no se repitan
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true
+    
+  },
+  name: { //nombre
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+},
+
+{
+    timestamps:false
+});
+};
---
+++ api/src/models/Recipe.js
@@ -0,0 +1,45 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('Recipe', {
+
+  id:{
+    type: DataTypes.UUID,  //esto es para no tener conflictos con los id's de la api que no se repitan
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true
+    
+  },
+  name: { //nombre
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+  summary: { //resumen
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+  healthscore: {//puntuacion
+    type: DataTypes.INTEGER,
+  },
+
+  img: { //imagen
+    type: DataTypes.STRING,
+    allowNull:false//'https://img.freepik.com/foto-gratis/cuaderno-receta-tomate-farfalle-ajo-cebolla-mortero-pimienta-vista-superior-copia-espacio_141793-3458.jpg'
+  },
+
+  steps: { //pasos
+    type:DataTypes.ARRAY(DataTypes.STRING)
+  },
+
+  dishtypes: { //platos
+    type: DataTypes.STRING
+  }
+
+  },
+
+  {
+      timestamps:false
+  });
+};
