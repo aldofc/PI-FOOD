19fc306d49970afab0d55dbb57caedb191571aaf
aldofc <aldofc18@gmail.com>
2023-02-27 15:50:39 -0600

    carpetas-rutas-front

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import LandingPage from './Pages/LandingPage/LandingPage';
 import Home from './Pages/Home/Home';
 import NavBar from './components/Navbar/Navbar';
 import Create from './Pages/Create/Create';
+import Details from './Pages/Details/Details';
 import { Route, Switch } from 'react-router-dom';
 function App() {
   return (
@@ -14,6 +15,9 @@ function App() {
 
         <Route exact path='/home' component={Home}/>
         <Route exact path='/create' component={Create}/>
+        <Route path='/home/:id' component={Details}/>
+
+
         
     </div>
   );
