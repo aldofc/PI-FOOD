16ae0eee3eb3b3037fb2817f2620eb57e040aba5
aldofc <aldofc18@gmail.com>
2023-03-05 15:03:21 -0600

    home-loader-errorHandler

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,19 +1,46 @@
 import React from 'react'
 import Card from '../../components/Card/Card'
 import NavBar from '../../components/Navbar/Navbar'
+import ErrorHandler from '../../components/Error-handler/ErrorHandler'
+import Loader from '../../components/Loader/Loader'
 import { useState, useEffect } from 'react'
 import { useDispatch, useSelector} from 'react-redux'
 
-import {getRecipes,getRecipeById,getRecipeByName,postRecipes,getDiets} from '../../../src/App/Actions/Index'
+import {getRecipes,getRecipeByName,getDiets} from '../../../src/App/Actions/Index'
 //import {Link} from 'react-router-dom'
 
 const Home = () => {
   
+const [searchString, setSearchString] = useState('')
+const [currentPage, setCurrentPage] = useState(1)
+const [cardsPerPage, setCardsPerpage] = useState(12)
+const [loading, setLoading] = useState(false)
+
 const dispatch = useDispatch();
 const diets = useSelector((state) => state.diets)
-const [searchString, setSearchString] = useState('')
+const dietsSorted = useSelector(state => state.dietsSorted)
+const error = useSelector(state => state.error)
 
+useEffect(() => {
+  setLoading(true)
+  dispatch(getRecipes())
+  dispatch(getDiets())
+  setTimeout(() => {
+    setLoading(false)
+  },2000)
+},[])
 
+const lastCardIndex = currentPage * cardsPerPage
+const firstCardIndex = lastCardIndex - cardsPerPage
+let currentCards
+if (dietsSorted.length > 0) {
+   currentCards = dietsSorted.slice(firstCardIndex, lastCardIndex)
+
+}else{
+  currentCards = diets.slice(firstCardIndex,lastCardIndex)
+}
+
+//----------------------------------------input
 function handleChange(e) {
   e.preventDefault(e)
   setSearchString(e.target.value)
@@ -33,14 +60,39 @@ useEffect(() => {
   dispatch(getRecipes())
 }, [dispatch])
 
+//-------------------------------------------input^
+
 return(
   <div><p>home</p>
     <NavBar 
     handleChange={handleChange}
     handleSubmit={handleSubmit}
     clearSearch={clearSearch}
-    
+    setCurrentPage={setCurrentPage}
+    setLoading={setLoading}
     />
+
+    <div>
+      { loading ? (
+        <Loader />
+      ) : error.length ? (
+        <ErrorHandler error={error} />
+      ) : (
+        currentCards?.map(e => {
+          console.log(e.createByUser)
+          return (
+            <Card
+            key={e.id}
+            id={e.id}
+            name={e.name}
+            image={e.image}
+            diets={e.diets}
+            healthscore={e.healthscore}
+            />
+          )
+        })
+      )}
+    </div>
 
     <Card diets={diets}/>
     
